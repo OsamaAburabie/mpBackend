863c17ccda1842c9f38686d2d4e7beea6b171658
OsamaAburabie <aburabie.osama@gmail.com>
2021-05-01 04:56:03 +0300

    started admin dashboard

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,6 +1,33 @@
 const Task = require("../models/taskModel");
 const User = require("../models/userModel");
 const Category = require("../models/categoryModel");
+const aws = require("aws-sdk");
+const fs = require("fs");
+// exports.add__category = async function (req, res) {
+//   try {
+//     const user = await User.findById(req.user);
+//     //checking if this user is an admin
+//     if (user.role != "admin")
+//       return res.status(400).json({ msg: "Unauthorized" });
+//     //================================
+//     const { name, picture, min, high } = req.body;
+//     //================================
+//     if (!name || !picture || !min || !high)
+//       return res.status(400).json({ msg: "all feilds are required" });
+
+//     const newCat = await Category({
+//       name,
+//       picture,
+//       min,
+//       high,
+//     });
+//     const category = await newCat.save();
+
+//     res.json(category);
+//   } catch (err) {
+//     res.status(404).json({ msg: err.message });
+//   }
+// };
 
 exports.add__category = async function (req, res) {
   try {
@@ -9,18 +36,124 @@ exports.add__category = async function (req, res) {
     if (user.role != "admin")
       return res.status(400).json({ msg: "Unauthorized" });
     //================================
-    const { name, picture, min, high } = req.body;
+    const { name, min, high } = req.body;
+    const img = req.file;
     //================================
-    if (!name || !picture || !min || !high)
+    if (!name || !min || !high || !img)
       return res.status(400).json({ msg: "all feilds are required" });
 
-    const newCat = await Category({
-      name,
-      picture,
-      min,
-      high,
+    //================================
+    aws.config.setPromisesDependency();
+    aws.config.update({
+      accessKeyId: process.env.ACCESSKEYID,
+      secretAccessKey: process.env.SECRETACCESSKEY,
+      region: process.env.REGION,
     });
-    const category = await newCat.save();
+
+    const s3 = new aws.S3();
+    var params = {
+      ACL: "public-read",
+      Bucket: process.env.BUCKET_NAME,
+      Body: fs.createReadStream(req.file.path),
+      Key: `userAvatar/${req.file.originalname}`,
+    };
+
+    s3.upload(params, async (err, data) => {
+      if (err) {
+        console.log("Error occured while trying to upload to S3 bucket", err);
+      }
+
+      if (data) {
+        fs.unlinkSync(req.file.path); // Empty temp folder
+        const locationUrl = data.Location;
+
+        const newCat = await Category({
+          name,
+          picture: locationUrl,
+          min,
+          high,
+        });
+        const category = await newCat.save();
+
+        res.json(category);
+      }
+    });
+  } catch (err) {
+    res.status(404).json({ msg: err.message });
+  }
+};
+
+exports.edit__category = async function (req, res) {
+  try {
+    const user = await User.findById(req.user);
+    //checking if this user is an admin
+    if (user.role != "admin")
+      return res.status(400).json({ msg: "Unauthorized" });
+    //================================
+    const { name, min, high } = req.body;
+    const img = req.file;
+    //================================
+    if (!name || !min || !high)
+      return res.status(400).json({ msg: "all feilds are required" });
+
+    const category = await Category.findOne({
+      _id: req.params.catId,
+    });
+
+    if (!img) {
+      await category.updateOne({ name, min, high });
+
+      const updated = await Category.findOne({
+        _id: req.params.catId,
+      });
+
+      res.json(updated);
+    } else if (img) {
+      //================================
+      aws.config.setPromisesDependency();
+      aws.config.update({
+        accessKeyId: process.env.ACCESSKEYID,
+        secretAccessKey: process.env.SECRETACCESSKEY,
+        region: process.env.REGION,
+      });
+
+      const s3 = new aws.S3();
+      var params = {
+        ACL: "public-read",
+        Bucket: process.env.BUCKET_NAME,
+        Body: fs.createReadStream(req.file.path),
+        Key: `userAvatar/${req.file.originalname}`,
+      };
+
+      s3.upload(params, async (err, data) => {
+        if (err) {
+          console.log("Error occured while trying to upload to S3 bucket", err);
+        }
+
+        if (data) {
+          fs.unlinkSync(req.file.path); // Empty temp folder
+          const locationUrl = data.Location;
+
+          await category.updateOne({ name, min, high, picture: locationUrl });
+
+          const updated = await Category.findOne({
+            _id: req.params.catId,
+          });
+
+          res.json(updated);
+        }
+      });
+    }
+  } catch (err) {
+    res.status(404).json({ msg: err.message });
+  }
+};
+
+exports.single_category = async function (req, res) {
+  try {
+    const category = await Category.findOne({
+      _id: req.params.catId,
+    });
 
     res.json(category);
   } catch (err) {
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,9 +1,32 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
-const { add__category } = require("../controllers/adminController");
+const {
+  add__category,
+  edit__category,
+  single_category,
+} = require("../controllers/adminController");
+const multer = require("multer");
 
-router.post("/addCategory", auth, add__category);
+router
+  .route("/addCategory")
+  .post(
+    multer({ dest: "temp/", limits: { fieldSize: 8 * 1024 * 1024 } }).single(
+      "img"
+    ),
+    auth,
+    add__category
+  );
+router
+  .route("/editCategory/:catId")
+  .put(
+    multer({ dest: "temp/", limits: { fieldSize: 8 * 1024 * 1024 } }).single(
+      "img"
+    ),
+    auth,
+    edit__category
+  );
 
+router.get("/singleCategory/:catId", single_category);
 // router.get("/allTickets", auth, get_all_tickets_by_admin);
 
 // router.put("/allTickets/:id", auth, change_ticket_status_by_admin);
